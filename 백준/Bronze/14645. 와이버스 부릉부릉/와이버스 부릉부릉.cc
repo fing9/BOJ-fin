52df8bd2769895281f8f08414987d07ac88d62df
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:28:20 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K;
+    cin >> N >> K;
+    for(int i=0;i<N;i++) {
+        int a, b;
+        cin >> a >> b;
+    }
+    cout << "비와이";
+
+    return 0;
+}
