701995c199f3139c616817be48e1c81153052144
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:22:24 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d, e, f;
+    
+    cin >> a >> b >> c >> d >> e >> f;
+    
+    cout << (a + b + c + d + e + f) * 5;
+    
+    return 0;
+}
