ad1785c876ece93d621184063471b7953710d3f3
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:28:59 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "     /~\\" << "\n";
+    cout << "    ( oo|" << "\n";
+    cout << "    _\\=/_" << "\n";
+    cout << "   /  _  \\" << "\n";
+    cout << "  //|/.\\|\\\\" << "\n";
+    cout << " ||  \\ /  ||" << "\n";
+    cout << "============" << "\n";
+    cout << "|          |" << "\n";
+    cout << "|          |" << "\n";
+    cout << "|          |" << "\n";
+
+    return 0;
+}
