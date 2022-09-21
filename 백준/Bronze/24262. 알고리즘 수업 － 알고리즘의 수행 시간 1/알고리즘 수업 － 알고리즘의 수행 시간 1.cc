bbfd89c8abd987b346b601462cfc0f4ff4afd728
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:27:56 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+
+    int n;
+    scanf("%d", &n);
+    printf("1\n0");
+
+    return 0;
+}
