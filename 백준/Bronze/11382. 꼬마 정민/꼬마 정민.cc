6427fef9764a94f77a50414c66b9793cae201e41
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:32:34 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long A, B, C;
+    
+    scanf("%llu %llu %llu", &A, &B, &C);
+    
+    printf("%llu", A+B+C);
+    
+    return 0;
+}
