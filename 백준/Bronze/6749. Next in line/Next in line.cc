c7f927cfaf6065beb551a42b024ac5fefe1fd3bf
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:26:50 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <algorithm>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+
+    int a, b;
+    
+    cin >> a;
+    cin >> b;
+    cout << b + b - a;
+
+    return 0;
+}
