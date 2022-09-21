038e2f36b8826c7094cb268b4edd38e7360a9bc8
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:28:13 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "I'm Sexy";
+
+    return 0;
+}
