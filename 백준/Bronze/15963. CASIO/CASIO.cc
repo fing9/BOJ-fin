a75adf9ec209fc96691ca1cb2ab1a3b7ff38a9b9
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2022-09-21 11:19:06 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long N, M;
+    cin >> N >> M;
+    
+    if(N == M) printf("1");
+    else printf("0");
+    
+    return 0;
+}
