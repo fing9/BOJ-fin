7c046b542d542593502ad88b0df348fda62b62e7
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2023-01-10 22:56:35 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4468 ms, Memory: 329972 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -1,79 +1,74 @@
+#pragma warning(disable:4996)
 #include <iostream>
 #include <algorithm>
+#include <utility>
 #include <vector>
-#include <cstdio>
-using namespace std;
-vector<long long> A, B, C, D, E;
-long long ret=0;
+#include <queue>
+#include <cmath>
+#define INF 1987654321
 
-void BinarySearch(long long target) {
-    long long lo=0, hi=E.size();
-    long long lower=-1, upper=-1;
-    int run1=0, run2=0;
-    
-    while(lo<hi) {
-        long long mid=(lo+hi)/2;
-        if(E[mid] < target) {
-            lo = mid+1;
-        }
-        else {
-            if(E[mid] == target)
-                run1 = 1;
-            hi = mid;
-        }
-    }
-    lower=hi;
-    
-    lo=0;
-    hi=E.size();
-    
-    while(lo<hi) {
-        long long mid=(lo+hi)/2;
-        if(E[mid] <= target) {
-            if(E[mid] == target)
-                run2 = 1;
-            lo = mid+1;
-        }
-        else {
-            hi = mid;
-        }
-    }
-    upper=hi;
-    
-    ret += upper - lower;
-    
-    return;
+using namespace std;
+
+long long A[4001], B[4001], C[4001], D[4001];
+vector<long long> AB, CD;
+int n;
+long long ret;
+
+long long BinarySearch(int value) {
+	long long lo = 0, hi = CD.size(), mid;
+	long long retLo, retHi;
+
+	while (lo < hi) {
+		mid = (lo + hi) / 2;
+
+		if (AB[mid] < value) {
+			lo = mid + 1;
+		}
+		else {
+			hi = mid;
+		}
+	}
+	retLo = hi;
+
+	lo = 0; hi = CD.size();
+	while (lo < hi) {
+		mid = (lo + hi) / 2;
+
+		if (AB[mid] <= value) {
+			lo = mid + 1;
+		}
+		else {
+			hi = mid;
+		}
+	}
+	retHi = lo;
+
+	return retHi - retLo;
 }
 
-int main() {
-    long long N;
-    scanf("%lld",&N);
-    A.resize(N);
-    B.resize(N);
-    C.resize(N);
-    D.resize(N);
-    
-    for(int i=0;i<N;i++) {
-        scanf("%lld %lld %lld %lld",&A[i],&B[i],&C[i],&D[i]);
-    }
-    
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end());
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            E.push_back(-(A[i]+B[j]));
-        }
-    }
-    sort(C.begin(),C.end());
-    sort(D.begin(),D.end());
-    sort(E.begin(),E.end());
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            BinarySearch(C[i]+D[j]);
-        }
-    }
-    
-    printf("%lld", ret);
-    
-    return 0;
+int main()
+{
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%lld %lld %lld %lld", &A[i], &B[i], &C[i], &D[i]);
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			AB.push_back(-(A[i] + B[j]));
+			CD.push_back(C[i] + D[j]);
+		}
+	}
+
+	sort(AB.begin(), AB.end());
+	sort(CD.begin(), CD.end());
+
+	for (int i = 0; i < CD.size(); i++) {
+		ret += BinarySearch(CD[i]);
+	}
+
+	printf("%lld", ret);
+
+	return 0;
 }
