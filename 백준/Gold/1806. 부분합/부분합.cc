b9200049a008925557f23798c72163a02794eb2b
Kim Seong Min <44383895+fing9@users.noreply.github.com>
2023-01-10 22:55:52 +0900

    [Gold IV] Title: 부분합, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,62 +1,43 @@
+#pragma warning(disable:4996)
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <utility>
-#include <queue>
-#include <stack>
-#include <cmath>
 #include <algorithm>
-#include <bitset>
-
-#define INF 1000000001
+#include <utility>
+#include <vector>
+#include <queue>
+#include <cmath>
+#define INF 1987654321
 
 using namespace std;
 
-int main() {
-//    ios_base::sync_with_stdio(false);
-//    cin.tie(NULL);
-//    cout.tie(NULL);
-    vector<int> v;
-    int n, s, x;
-    scanf("%d %d", &n, &s);
-    v.push_back(0);
-    for(int i=1;i<=n;i++) {
-        scanf("%d", &x);
-        v.push_back(x + v[i-1]);
-        //printf("%d ", v[i]);
-    }
-    //printf("\n");
+vector<int> pSum(100001);
+int N, S, ret = INF;
+int s=0, e=1;
 
-    int ret = INF;
-    int l = 0, r = 1;
-    while(r < n+1)
-    {
-        if(v[r] - v[l] == s)
-        {
-            if(ret > r - l)
-                ret = r - l;
-            r++;
-        }
-        else if(v[r] - v[l] < s)
-        {
-            r++;
-        }
-        else
-        {
-            if(ret > r - l)
-                ret = r - l;
-            if(l < r)
-                l++;
-            else
-                r++;
-        }
-    }
+int main()
+{
+	scanf("%d %d", &N, &S);
 
-    if(ret != INF)
-        printf("%d", ret);
-    else
-        printf("0");
+	for (int i = 1; i <= N; i++) {
+		int inp;
+		scanf("%d", &inp);
 
-    return 0;
+		pSum[i] = pSum[i - 1] + inp;
+	}
+
+	while (s<=N && e<=N) {
+		int sum = pSum[e] - pSum[s];
+
+		if (sum >= S) {
+			ret = min(ret, e - s);
+			s++;
+		}
+		else {
+			e++;
+		}
+	}
+
+	if (ret == INF) printf("0");
+	else printf("%d", ret);
+
+	return 0;
 }
