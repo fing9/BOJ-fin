5fc2c9d2131e2cc2c250b01f5c4f3be0894d57bb
Seongmin Kim <44383895+fing9@users.noreply.github.com>
2023-07-03 16:09:41 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    printf("2000\n");
+    for (int i=0;i<1000;i++) {printf("1000 ");}
+    for (int i=0;i<1000;i++) {printf("1 ");}
+    return (0);
+} 
