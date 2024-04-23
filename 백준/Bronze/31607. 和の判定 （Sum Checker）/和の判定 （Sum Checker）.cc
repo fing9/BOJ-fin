82e124d82926a15b40538fcfc0e18812734f1e2c
Seongmin Kim <44383895+fing9@users.noreply.github.com>
2024-04-23 20:16:43 +0900

    [Bronze IV] Title: 和の判定 (Sum Checker), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31607. 和の判定 （Sum Checker）/和の判定 （Sum Checker）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c || a + c == b || b + c == a) {
+        cout << "1";
+    } else {
+        cout << "0";
+    }
+}
